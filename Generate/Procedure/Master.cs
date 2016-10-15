5d83927b1d7c7c9205abd797337bf57b8ef0028f
Amir Zaidi <azaidi@live.nl>
2016-10-15 15:32:34 +0200

    Testing seed combination algorhytms

---
+++ Generate/Procedure/Master.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generate.Procedure
+{
+    class Master
+    {
+        private byte[] Seed;
+
+        internal Master(byte[] Seed)
+        {
+            this.Seed = new byte[Seed.Length + 4];
+            Seed.CopyTo(this.Seed, 4);
+
+            this.Seed[0] = (byte)(Seed.Length & 0xff);
+            this.Seed[1] = (byte)((Seed.Length >> 8) & 0xff);
+            this.Seed[2] = (byte)((Seed.Length >> 16) & 0xff);
+            this.Seed[3] = (byte)((Seed.Length >> 24) & 0xff);
+        }
+
+        internal int GetSeed(byte[] For)
+        {
+            int Stride = 0;
+            for (int i = 0; i < For.Length; i++)
+            {
+                Stride += For[i];
+            }
+
+            Stride %= Seed.Length;
+
+            var Result = new byte[4];
+            for (int i = 0; i < For.Length; i++)
+            {
+                int Place = For[i] * Stride % Seed.Length;
+                Result[i % 4] ^= Seed[Place];
+            }
+            
+            return BitConverter.ToInt32(Result, 0);
+        }
+    }
+}
--- Generate/Program.cs
+++ Generate/Program.cs
@@ -10,7 +10,24 @@ namespace Generate
     {
         static void Main(string[] args)
         {
+            Log("Seed? ");
+            Procedure.Worker.Master = new Procedure.Master(Encoding.ASCII.GetBytes(Console.ReadLine()));
+            LogLine(Procedure.Worker.Master.GetSeed(Encoding.ASCII.GetBytes("Main")));
+            LogLine(Procedure.Worker.Master.GetSeed(Encoding.ASCII.GetBytes("Slave")));
+            LogLine(Procedure.Worker.Master.GetSeed(Encoding.ASCII.GetBytes("Depth")));
+            LogLine(Procedure.Worker.Master.GetSeed(Encoding.ASCII.GetBytes("Colors")));
+            LogLine(Procedure.Worker.Master.GetSeed(Encoding.ASCII.GetBytes("Test A Longer String LOL")));
+            LogLine(Procedure.Worker.Master.GetSeed(Encoding.ASCII.GetBytes("¥¬")));
 
+            Log(new Random(Procedure.Worker.Master.GetSeed(Encoding.ASCII.GetBytes("negative"))).Next());
+
+            Console.ReadLine();
         }
+
+        static void LogLine(object In, string From = null)
+            => Log(In + "\r\n", From);
+
+        static void Log(object In, string From = null)
+            => Console.Write($"[{DateTime.Now.ToLongTimeString()}] {From ?? "Main"} - {In}");
     }
 }
